0dbe27b84686a8373333c9c9b74c2797901f06f3
phenix1229 <paul.garay@codeimmersives.com>
2019-10-17 13:56:33 -0400

    adding links to pics

--- main.js
+++ main.js
@@ -5,23 +5,30 @@
 //     return document.querySelector(img).appendChild(iLink);
 // }
 
-document.querySelectorAll('.thumbnail').forEach(function (space){
+// 
+
+document.querySelectorAll('.img1').forEach(function (space){
+    const 
+    const picInfo = document.querySelector('.img1').innerHTML
+    const pic = document.querySelector('.img1 > img')
     const iLink = document.createElement('a');
     iLink.target = '_blank';
-    iLink.href = space.src; 
-    iLink.innerText = `<img src='space.src' height="120px"></img>`;
+    iLink.href = pic.src; 
+    iLink.innerHTML = picInfo;
+    ;
+    
     return space.appendChild(iLink);
 })
 
-function testLink(){
-    const testLink = document.createElement('a')
-    testLink.href = "#";
-    testLink.innerText = "test link";
-    testLink.id = "test";
-    return document.body.appendChild(testLink);
-}
+// function testLink(){
+//     const testLink = document.createElement('a')
+//     testLink.href = "#";
+//     testLink.innerText = "test link";
+//     testLink.id = "test";
+//     return document.body.appendChild(testLink);
+// }
 
-testLink()
+// testLink()
 // document.body.appendChild('#test')
 
 // addLinkToThumb('.thumbnail');
