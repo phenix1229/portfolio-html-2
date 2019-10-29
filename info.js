cdd67e58f8cd0f51691b3da9f40689981ad8ffe4
phenix1229 <paul.garay@codeimmersives.com>
2019-10-29 15:55:59 -0400

    yourInfo app working

---
+++ info.js
@@ -0,0 +1,8 @@
+const birthDate = prompt("What is your birth date (mmddyyyy)?\n")
+
+const optionNum = prompt("What would you like to know?\n1 = Age\n2 = Zodiac sign\n3 = Birthstone\n4 = All of the above\nPress any other key and 'Enter' to exit\n")
+
+const result = yourInfo(optionNum);
+  
+
+ document.querySelector('#result').innerText = result
--- yourInfo.js
+++ yourInfo.js
@@ -90,34 +90,10 @@ function yourInfo(optionNum){
   } else if (optionNum === "3"){
     return birthStone(birthDate);
   } else if(optionNum === '4'){
-    return age(birthDate), zodiacSign(birthDate), birthStone(birthDate);
+    return `${age(birthDate)}\n${zodiacSign(birthDate)}\n ${birthStone(birthDate)}`;
   } else {
     return "Goodbye"}
 }
-
-const readline =
-require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-
-readline.question("What is your birth date (mmddyyyy)?\n",
-  (birthDate) => {
-    readline.setPrompt("What would you like to know?\n1 = Age\n2 = Zodiac sign\n3 = Birthstone\n4 = All of the above\nPress any other key and 'Enter' to exit\n");
-    readline.prompt();
-    readline.on('line', (userInput) => {
-      if (optionNum === "1"){
-        console.log(age(birthDate));
-      } else if (optionNum === "2"){
-        console.log(zodiacSign(birthDate));
-      } else if (optionNum === "3"){
-        console.log(birthStone(birthDate));
-      } else if(optionNum === '4'){
-        console.log(age(birthDate), zodiacSign(birthDate), birthStone(birthDate));
-      } else {
-        console.log("Goodbye")}
-      readline.close()})
-  })
 
 module.exports = {
   separateDate,
